6e27efc472fbe5a29ce44cb2f5363adfab888e76
qejmer <unaymazlum@gmail.com>
2025-11-08 10:04:03 -0500

    Initial commit

---
+++ 293-keeper-app-challenge/src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Note from "./Note.jsx";
+import Footer from "./Footer.jsx";
+import notes from "../notes.js";
+
+
+
+
+function App(){
+    return (
+        <div>
+            <Header />
+            {notes.map((noteItem) =>
+             < Note 
+                 key = {noteItem.key}
+                title = {noteItem.title}
+                content = {noteItem.content}
+             />
+            )}
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
