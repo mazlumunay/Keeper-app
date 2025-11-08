49484aa7be11561e12c276c3e2d481cb3b5d112a
qejmer <unaymazlum@gmail.com>
2025-11-08 11:07:48 -0500

    fix add/delete functionality

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,28 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
-  const [note,setNote] = useState({
-    title: " ",
-    content:" "
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
   });
 
-  function handleChange(event){
-    const {name,value} = event.target;
+  function handleChange(event) {
+    const { name, value } = event.target;
 
     setNote(prevNote => {
-      return{
+      return {
         ...prevNote,
-        [name]:value
+        [name]: value
       };
     });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
   }
 
   return (
     <div>
       <form>
-        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
-        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
